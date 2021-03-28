909fb80947af103883c1d3795e6b8a9f5cb6a355
r-tamura-omniment <rui-tamura@omniment.co.jp>
2021-03-29 05:11:46 +0900

    [add]example追加

---
+++ libraries/RECoreLibrary/examples/Parallel_DC_example/Parallel_DC_example.ino
@@ -0,0 +1,52 @@
+/*
+   RECore Library
+   Parallel DC motor example
+
+   All motor unit use parallel DC motor mode.
+
+*/
+
+#include <RECoreLibrary.h>
+
+RECoreLibrary recore;
+
+void setup() {
+  //init motor type parallel_dc mode
+  recore.setMotorType(unit_a, parallel_dc);
+  recore.setMotorType(unit_b, parallel_dc);
+
+  //set all motor current limit
+  //setMotorCurrent(current);
+  //current : 1 ~ 1500
+  
+  //example set 500mA
+  recore.setMotorCurrent(500);
+
+  //set motor brake mode
+  //can change motor brake mode
+  //setBrakeMode(motor number, brake type);
+  //motor number : 0,2 (Parallel mode can set 0 or 2)
+  //brake type : brake , coast (default set brake)
+}
+
+void loop() {
+  //set Motor speed
+  //setMotorSpeed(motor number, motor power);
+  //motor number : 0 , 2
+  //motor power : -1.0 ~ 0 ~ 1.0
+  
+  //example set Motor 0 speed forward 50%
+  recore.setMotorSpeed(0, 0.5);
+  delay(1000);
+
+  //stop motor 0
+  recore.setMotorSpeed(0, 0);
+  delay(1000);
+
+  //set Motor 0 speed reverse 50%
+  recore.setMotorSpeed(0, -0.5);
+  delay(1000);
+
+  recore.setMotorSpeed(0, 0);
+  delay(1000);
+}
